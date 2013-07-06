b25dc0b73889bb73ecf0d9b8c3cc32e631f016b6
Itay Karo <karoitay@gmail.com>
2013-07-06 13:54:46 +0300

    Create ProgrammingAssignmentWeek3.py

---
+++ ProgrammingAssignmentWeek3.py
@@ -0,0 +1,24 @@
+import os
+from Crypto.Hash import SHA256
+
+BLOCK_SIZE = 1024
+FILE_NAME = "filename"
+
+def blocks(f):
+  f.seek(0, os.SEEK_END)
+  next_block_size = f.tell() % BLOCK_SIZE
+  while f.tell() > 0:
+    f.seek(-next_block_size, os.SEEK_CUR)
+    yield f.read(next_block_size)
+    f.seek(-next_block_size, os.SEEK_CUR)
+    next_block_size = BLOCK_SIZE
+
+def h0(f):
+  last_sha = ""
+  for b in blocks(f):
+    last_sha = SHA256.new(b + last_sha).hexdigest().decode("hex")
+  return last_sha
+  
+with open(FILE_NAM, "rb") as f:
+  print h0(f).encode("hex")
+
